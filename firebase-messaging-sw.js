224c7f85d4619727dc5adbd37d918ef381c8e4de
Tejaswini Khandekar <90246533+teekhandekar@users.noreply.github.com>
2023-05-17 19:10:59 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,12 +2,12 @@ importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.6.5/firebase-messaging.js');
 
 const firebaseConfig = {
-    apiKey: 'AIzaSyCYcHCdgc-tCeRXHRnZ8uVKENidMaHRL9s',
-  authDomain: 'chat-application-611b3.firebaseapp.com',
-  projectId: 'chat-application-611b3',
-  storageBucket: 'chat-application-611b3.appspot.com',
-  messagingSenderId: '284177842733',
-  appId: '1:284177842733:web:e84335588fc1798f1a2ea4'
+    apiKey: 'ApiKey',
+  authDomain: 'yourAuthDomain',
+  projectId: 'projectId',
+  storageBucket: 'storagrBucket',
+  messagingSenderId: 'senderId',
+  appId: 'appId'
 };
 
 firebase.initializeApp(firebaseConfig);
